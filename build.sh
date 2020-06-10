210115060de23d38fb86457f64b739b7ec21d4df
Stewart Smith <stewart.dryden.smith@gmail.com>
2020-06-09 20:33:39 -0700

    Add shortcut for debug builds

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+BUILD_MODE=""
+
+if [[ ! -z "$1" ]] ; then
+    case "$1" in
+        "debug") BUILD_MODE="-DCMAKE_BUILD_TYPE=Debug" ;;
+    esac
+fi
+
 mkdir -p artifacts >/dev/null
 pushd . >/dev/null
 cd artifacts >/dev/null
 # Config step: point to CMakeLists.txt
-cmake ..
+cmake ${BUILD_MODE} ..
 # Build: point to cache from config
-cmake $* --build .
+cmake --build .
 popd >/dev/null
